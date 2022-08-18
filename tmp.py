da64a783f2f225651be7a4fa37f9339853b35ca6
FriLDD <dotpointen@gmail.com>
2022-08-18 22:48:29 +0300

    entropy calc

--- tmp.py
+++ tmp.py
@@ -1,6 +1,7 @@
-class A:
-    def __init__(self) -> None:
-        pass
-    
-    def __del__(self):
-        print(f"dell")
+from math import log2
+
+
+def entropy(p):
+    return -p * log2(p)
+
+print(entropy(4/5) + entropy(1/5))
